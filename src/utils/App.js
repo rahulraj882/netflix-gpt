4bf37a18a395bb45a97bd20353991b03a49e78d8
Rahul Raj <rahul800902@gmail.com>
2025-10-05 14:41:22 +0530

    login and header

---
+++ src/utils/App.js
@@ -0,0 +1,14 @@
+import Login from '../components/Login.js'
+import Body from '../components/Body.js'
+import Header from '../components/Header.js'
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+    
+  );
+} 
+
+export default App;
